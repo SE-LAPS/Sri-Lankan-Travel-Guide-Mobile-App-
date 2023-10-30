b7368dd7e9e9c1320eb30de32c389647e3330eaa
Lahiru Senavirathna <87580847+SE-LAPS@users.noreply.github.com>
2023-10-30 08:11:34 +0530

    add Colors.dart file

---
+++ constants/Colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff303236);
+const secondColor = Color(0xff3d3f43);
+const greenColor = Color(0xff1ed760);
+const scaffoldBackgroundColor = Color(0xff303236);
+const tabBarLabelColor = Colors.white;
+const tabBarUnderlineColor = Colors.white;
