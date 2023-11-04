e2e9b1890665887e5894402359d8355f1dd6d9fa
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-11-05 00:00:17 +0530

    Create myStrings.dart

---
+++ global/myStrings.dart
@@ -0,0 +1,50 @@
+class MyStrings {
+  static const appNameUppercase = "KYT";
+  static const appNameLowercase = "Kyt";
+  static const tagline = "Know your traveller.";
+
+  // auth region starts
+  static const joinKytLabel = "Join KYT";
+  static const loginLabel = "Login";
+  static const registerLabel = "Register";
+  static const or = "or";
+  static const createNewAccountLabel = "Create new account";
+  static const loggingInLabel = "Logging in";
+  static const registeringLabel = "Registering";
+
+  static const loginToKYT = "Login to KYT";
+  static const registerForKYT = "Register for KYT";
+  static const emailAddressLabel = "Email address";
+  static const passwordLabel = "Password";
+  static const passportLabel = "Passport number";
+  static const noAccountLabel = "Don't have an account yet?";
+
+  static const nameLabel = "Name";
+  static const aadharNumberLabel = "Aadhar card number";
+  static const phoneNumberLabel = "Phone number";
+  static const dateOfBirth = "Date of birth";
+  static const alreadyAnAccountLabel = "Already have an account?";
+  // auth region ends
+
+  // auth error starts
+  static const userNotFoundError = "User not found.";
+  static const incorrectPasswordError = "Incorrect Password";
+  static const somethingWentWrongError = "Something went wrong";
+  static const emailError = "Invalid email address entered.";
+  static const passwordRequiredError = "Password is required.";
+  static const passwordLengthError =
+      "Password must be at least 6 characters long.";
+  static const passwordMismatchError = "Passwords do not match";
+  static const phoneNumberError = "Phone number is required.";
+  static const passportNumberError = "Invalid passport number entered.";
+  // auth error ends
+
+  // home starts
+  static const travellerLabel = "Traveller";
+  static const sampleName = "Sayam Kanwar";
+  static const verificationLabel = "Verification";
+  static const test1Label = "Covid Vaccine";
+  static const test2Label = "Covid Test";
+  static const test3Label = "Test 3";
+  static const test4Label = "Test 4";
+}
