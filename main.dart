3cffc3c0253252b9990cf939d37871bd6cc2b431
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-11-06 16:19:38 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,27 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:travel/constants.dart';
-import 'package:travel/screens/home/home_screen.dart';
+import 'package:login_and_signup/login.dart';
+import 'package:login_and_signup/login_option.dart';
+import 'package:login_and_signup/signup.dart';
+import 'package:login_and_signup/signup_option.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Travels App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        brightness: Brightness.light,
-        scaffoldBackgroundColor: Colors.white,
-        textTheme:
-            GoogleFonts.poppinsTextTheme().apply(displayColor: kTextColor),
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.muktaVaaniTextTheme(),
       ),
-      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  bool login = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1C1C1C),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  login = true;
+                });
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.ease,
+                height: login ? MediaQuery.of(context).size.height * 0.6 : MediaQuery.of(context).size.height * 0.4,
+                child: CustomPaint(
+                  painter: CurvePainter(login),
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: login ? 0 : 55),
+                    child: Center(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                          child: login 
+                          ? Login()
+                          : LoginOption(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  login = false;
+                });
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.ease,
+                height: login ? MediaQuery.of(context).size.height * 0.4 : MediaQuery.of(context).size.height * 0.6,
+                child: Container(
+                  color: Colors.transparent,
+                  padding: EdgeInsets.only(top: login ? 55 : 0),
+                  child: Center(
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                        child: !login 
+                        ? SignUp()
+                        : SignUpOption(),
+                      ),
+                    ),
+                  )
+                ),
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CurvePainter extends CustomPainter {
+
+  bool outterCurve;
+
+  CurvePainter(this.outterCurve);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Color(0xFFF3D657);
+    paint.style = PaintingStyle.fill;
+
+    Path path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(size.width * 0.5, outterCurve ? size.height + 110 : size.height - 110, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
